2a0477c89c1f98e26e8d477c6ee711ec8edada5a
wootaik Lee <fined000@naver.com>
2024-06-27 15:54:36 +0900

    lint:fix

--- src/remote/map/subway/getKakaoAddr.ts
+++ src/remote/map/subway/getKakaoAddr.ts
@@ -1,8 +1,10 @@
-import baseApiInstance from "./baseURL"
+import baseApiInstance from './baseURL'
 
 export const getKakaoAddr = async (keyword: string) => {
   try {
-    const response = await baseApiInstance.get(`/v2/local/search/address.json?query=${keyword}&page=1&size=15`)
+    const response = await baseApiInstance.get(
+      `/v2/local/search/address.json?query=${keyword}&page=1&size=15`,
+    )
     return response.data.documents
   } catch (error) {
     console.error(error)
